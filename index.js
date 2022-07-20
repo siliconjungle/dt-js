32e9a26a14bf202114843dc145ac6c4a300efddb
Seph Gentle <me@josephg.com>
2022-07-20 22:25:58 +1000

    Moved example code out of index.js

--- index.js
+++ index.js
@@ -1,17 +1,2 @@
 import Storage, { ROOT } from './Storage.js'
-
-const storage = new Storage()
-
-const operations = [
-  ['agent1', 1, [], ROOT, 'map', 'key', 'value1'],
-  ['agent3', 2, [], ROOT, 'map', 'key', 'value3'],
-  ['agent2', 3, [], ROOT, 'map', 'key', 'value2'],
-]
-
-storage.on('applylocal', (ops) => {
-  console.log('Apply local', ops)
-})
-
-storage.applyOperations(operations, true)
-
-console.log('Storage value', storage.value)
+export {Storage, ROOT}
